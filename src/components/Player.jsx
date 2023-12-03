6ca7d6e9da318fca70904ac216c2c9a0ea7e34f8
Shayaan Siddiqui <shayaan.siddiqui@gmail.com>
2023-12-03 11:14:21 -0500

    Player component using useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,24 @@
+import {useState} from "react";
+
 export default function Player() {
-  return (
-    <section id="player">
-      <h2>Welcome unknown entity</h2>
-      <p>
-        <input type="text" />
-        <button>Set Name</button>
-      </p>
-    </section>
-  );
+    const [enteredPlayerName, setEnteredPlayerName] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+
+    const handleChange = (event) => {
+        setSubmitted(false);
+        setEnteredPlayerName(event.target.value);
+    }
+
+    const handleClick = () => {
+        setSubmitted(true);
+    }
+    return (
+        <section id="player">
+            <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+            <p>
+                <input type="text" onChange={handleChange} value={enteredPlayerName}/>
+                <button onClick={handleClick}>Set Name</button>
+            </p>
+        </section>
+    );
 }
