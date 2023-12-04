3f0e74b2f2f5d44c13ad2a310287628c33e9f648
shayaansiddiqui <shayaan.siddiqui@gmail.com>
2023-12-04 11:37:15 -0500

    updated input in Player to use Ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,26 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 export default function Player() {
+    const playerName = useRef();
     const [enteredPlayerName, setEnteredPlayerName] = useState('');
-    const [submitted, setSubmitted] = useState(false);
+    // const [submitted, setSubmitted] = useState(false);
 
-    const handleChange = (event) => {
-        setSubmitted(false);
-        setEnteredPlayerName(event.target.value);
-    }
+    // const handleChange = (event) => {
+    //     setSubmitted(false);
+    //     setEnteredPlayerName(event.target.value);
+    // }
 
     const handleClick = () => {
-        setSubmitted(true);
+        // setSubmitted(true);
+        setEnteredPlayerName(playerName.current.value);
+        playerName.current.value = '';
     }
+
     return (
         <section id="player">
-            <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+            <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
             <p>
-                <input type="text" onChange={handleChange} value={enteredPlayerName}/>
+                <input type="text" ref={playerName}/>
                 <button onClick={handleClick}>Set Name</button>
             </p>
         </section>
