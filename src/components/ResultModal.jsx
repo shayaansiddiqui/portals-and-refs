fa64ecba47518519ae8a7fcad247b98c84267c0b
shayaansiddiqui <shayaan.siddiqui@gmail.com>
2023-12-05 16:11:33 -0500

    Using ImerativeRef

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,7 +1,12 @@
 import {forwardRef, useRef, useImperativeHandle} from 'react';
 
-const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
+const ResultModal = forwardRef(function ResultModal({result, targetTime, remainingTime, onReset}, ref) {
 	const dialog = useRef();
+	const formattedRemainingTime = (remainingTime/1000).toFixed(2);
+
+	const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+	const userLost = remainingTime <= 0;
 	useImperativeHandle(ref, () => {
 		return {
 			open() {
@@ -10,11 +15,12 @@ const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
 		};
 	});
 	return (
-		<dialog ref={dialog} className={'result-modal'}>
-			<h2>You {result}!</h2>
+		<dialog ref={dialog} className={'result-modal'} onClose={onReset}>
+			{userLost && <h2>You lost!</h2>}
+			{!userLost && <h2>Your score: {score}</h2>}
 			<p>The target time was <strong>{targetTime} seconds.</strong></p>
-			<p>You stopped the timer with <strong>X seconds left</strong></p>
-			<form method={'dialog'}>
+			<p>You stopped the timer with <strong>{formattedRemainingTime} seconds left</strong></p>
+			<form method={'dialog'} onSubmit={onReset}>
 				<button>Close</button>
 			</form>
 		</dialog>
